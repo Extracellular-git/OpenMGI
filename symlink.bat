6b77205f0c972279dae183ad0abb220596da8c30
AlexT <alext@extracellular.com>
2024-03-21 14:43:38 +0000

    add: kit image config to symlink.bat

--- symlink.bat
+++ symlink.bat
@@ -1,3 +1,4 @@
 mklink C:\MGISP-960\Engineer\Config\Sp96\kit.json C:\Users\MGI_1\Projects\OpenMGI\config\kit.json
 mklink C:\MGISP-960\Engineer\Config\Sp96\ConsumablesCategory.json C:\Users\MGI_1\Projects\OpenMGI\config\ConsumablesCategory.json
 mklink C:\MGISP-960\Engineer\Config\DesignerConfig\category.json C:\Users\MGI_1\Projects\OpenMGI\config\category.json
+mklink /D C:\MGISP-960\Engineer\Config\DesignerConfig\consumables C:\Users\MGI_1\Projects\OpenMGI\config\consumables
